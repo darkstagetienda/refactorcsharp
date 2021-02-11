56a2243d4dca1af7bc8177353a7a8fa4f0a4d818
Dark <darkstagetienda@gmail.com>
2021-02-11 17:40:15 -0500

    adding files

---
+++ ConsoleApp/Account.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp
+{
+    public class Account
+    {
+        private bool IsVerified { get; set; }
+        private bool IsClosed { get; set; }
+        public void Deposit(decimal amount)
+        {
+            if (this.IsClosed)
+                return;
+            //deposit money
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (!this.IsVerified)
+                return; //or do something
+            //withdraw mone
+        }
+
+        public void HoldVerified()
+        {
+            this.IsVerified = true;
+        }
+
+        public void Close()
+        {
+            this.IsClosed = true;
+        }
+    }
+}
